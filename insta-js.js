fa5a921eb42a2f8db9600c80e4afe54525b7c5e6
gramos <imeri.gramos@gmail.com>
2018-04-18 15:33:28 +0200

    Modification Gramos

---
+++ insta-js.js
@@ -0,0 +1,141 @@
+$(function() {
+
+  var closeBtn = $(".fermeFenetre");
+
+  $("#imgProfil-1").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-1").attr("src", $changeSrc);
+    $("#nouvelleFenetre-1").css("display", "block");
+  });
+
+  $("#imgProfil-2").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-2").attr("src", $changeSrc);
+    $("#nouvelleFenetre-2").css("display", "block");
+  });
+
+  $("#imgProfil-3").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-3").attr("src", $changeSrc);
+    $("#nouvelleFenetre-3").css("display", "block");
+  });
+
+  $("#imgProfil-4").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-4").attr("src", $changeSrc);
+    $("#nouvelleFenetre-4").css("display", "block");
+  });
+
+  $("#imgProfil-5").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-5").attr("src", $changeSrc);
+    $("#nouvelleFenetre-5").css("display", "block");
+  });
+
+  $("#imgProfil-6").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-6").attr("src", $changeSrc);
+    $("#nouvelleFenetre-6").css("display", "block");
+  });
+
+  $("#imgProfil-7").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-7").attr("src", $changeSrc);
+    $("#nouvelleFenetre-7").css("display", "block");
+  });
+
+  $("#imgProfil-8").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-8").attr("src", $changeSrc);
+    $("#nouvelleFenetre-8").css("display", "block");
+  });
+
+  $("#imgProfil-9").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-9").attr("src", $changeSrc);
+    $("#nouvelleFenetre-9").css("display", "block");
+  });
+
+  $("#imgProfil-10").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-10").attr("src", $changeSrc);
+    $("#nouvelleFenetre-10").css("display", "block");
+  });
+
+  $("#imgProfil-11").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-11").attr("src", $changeSrc);
+    $("#nouvelleFenetre-11").css("display", "block");
+  });
+
+  $("#imgProfil-12").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-12").attr("src", $changeSrc);
+    $("#nouvelleFenetre-12").css("display", "block");
+  });
+
+  $("#imgProfil-13").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-13").attr("src", $changeSrc);
+    $("#nouvelleFenetre-13").css("display", "block");
+  });
+
+  $("#imgProfil-14").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-14").attr("src", $changeSrc);
+    $("#nouvelleFenetre-14").css("display", "block");
+  });
+
+  $("#imgProfil-15").click(function() {
+    var $changeSrc = $(this).attr("src");
+    $("#testons-15").attr("src", $changeSrc);
+    $("#nouvelleFenetre-15").css("display", "block");
+  });
+
+
+  closeBtn.click(function() {
+    $("#nouvelleFenetre-1").css("display", "none")
+    $("#nouvelleFenetre-2").css("display", "none")
+    $("#nouvelleFenetre-3").css("display", "none")
+    $("#nouvelleFenetre-4").css("display", "none")
+    $("#nouvelleFenetre-5").css("display", "none")
+    $("#nouvelleFenetre-6").css("display", "none")
+    $("#nouvelleFenetre-7").css("display", "none")
+    $("#nouvelleFenetre-8").css("display", "none")
+    $("#nouvelleFenetre-9").css("display", "none")
+    $("#nouvelleFenetre-10").css("display", "none")
+    $("#nouvelleFenetre-11").css("display", "none")
+    $("#nouvelleFenetre-12").css("display", "none")
+    $("#nouvelleFenetre-13").css("display", "none")
+    $("#nouvelleFenetre-14").css("display", "none")
+    $("#nouvelleFenetre-15").css("display", "none")
+  })
+
+  $(document).click(function(e) {
+    if ((e.target.id == "nouvelleFenetre-1") || (e.target.id == "nouvelleFenetre-2") || (e.target.id == "nouvelleFenetre-3") ||
+      (e.target.id == "nouvelleFenetre-4") || (e.target.id == "nouvelleFenetre-5") ||
+      (e.target.id == "nouvelleFenetre-6") || (e.target.id == "nouvelleFenetre-7") ||
+      (e.target.id == "nouvelleFenetre-8") || (e.target.id == "nouvelleFenetre-9") ||
+      (e.target.id == "nouvelleFenetre-10") || (e.target.id == "nouvelleFenetre-11") ||
+      (e.target.id == "nouvelleFenetre-12") || (e.target.id == "nouvelleFenetre-13") ||
+      (e.target.id == "nouvelleFenetre-14") || (e.target.id == "nouvelleFenetre-15")) {
+      $("#nouvelleFenetre-1").css("display", "none")
+      $("#nouvelleFenetre-2").css("display", "none")
+      $("#nouvelleFenetre-3").css("display", "none")
+      $("#nouvelleFenetre-4").css("display", "none")
+      $("#nouvelleFenetre-5").css("display", "none")
+      $("#nouvelleFenetre-6").css("display", "none")
+      $("#nouvelleFenetre-7").css("display", "none")
+      $("#nouvelleFenetre-8").css("display", "none")
+      $("#nouvelleFenetre-9").css("display", "none")
+      $("#nouvelleFenetre-10").css("display", "none")
+      $("#nouvelleFenetre-11").css("display", "none")
+      $("#nouvelleFenetre-12").css("display", "none")
+      $("#nouvelleFenetre-13").css("display", "none")
+      $("#nouvelleFenetre-14").css("display", "none")
+      $("#nouvelleFenetre-15").css("display", "none")
+
+
+    }
+  });
+});
