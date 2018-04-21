6d88a9f77528ecfcb3f0491eba4770d42eed9046
gramos <imeri.gramos@gmail.com>
2018-04-21 14:57:07 +0200

    navbar reduite scroll

--- insta-js.js
+++ insta-js.js
@@ -3,108 +3,41 @@ $(function() {
 
   var closeBtn = $(".fermeFenetre");
 
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $('.logo1-img').css("width", "30%");
+      $(".logo2-img").css("width", "100px");
+      $(".recherche-input").css("height", "15px");
+      $(".nav").css("padding-top", "0");
+      $(".nav").css("max-height", "55px");
+      $(".nav-ul").css("margin-left", "150px");
+    } else {
+      $('.logo1-img').css("width", "45%");
+      $('.logo2-img').css("width", "150px");
+      $(".recherche-input").css("height", "25px");
+      $(".nav").css("padding-top", "0.25%");
+      $(".nav").css("max-height", "100px");
+      $(".nav-ul").css("margin-left", "0px");
+    }
+  })
+  
+  $(".button-icons").click(function(){
+    $(".modal-icons-1").css("color",'red');
+  });
 
   $(".js--img").click(function() {
     var $changeSrc = $(this).attr("src");
     var modal = $(this).data('modal');
-    $("#testons-"+modal).attr("src", $changeSrc);
-    $("#nouvelleFenetre-"+modal).css("display", "block");
+    $("#testons-" + modal).attr("src", $changeSrc);
+    $("#nouvelleFenetre-" + modal).css("display", "block");
   });
-
-  $("#imgProfil-2").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-2").attr("src", $changeSrc);
-    $("#nouvelleFenetre-2").css("display", "block");
-  });
-
-  $("#imgProfil-3").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-3").attr("src", $changeSrc);
-    $("#nouvelleFenetre-3").css("display", "block");
-
-  });
-
-  $("#imgProfil-4").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-4").attr("src", $changeSrc);
-    $("#nouvelleFenetre-4").css("display", "block");
-
-  });
-
-  $("#imgProfil-5").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-5").attr("src", $changeSrc);
-    $("#nouvelleFenetre-5").css("display", "block");
-  });
-
-  $("#imgProfil-6").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-6").attr("src", $changeSrc);
-    $("#nouvelleFenetre-6").css("display", "block");
-  });
-
-  $("#imgProfil-7").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-7").attr("src", $changeSrc);
-    $("#nouvelleFenetre-7").css("display", "block");
-  });
-
-  $("#imgProfil-8").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-8").attr("src", $changeSrc);
-    $("#nouvelleFenetre-8").css("display", "block");
-  });
-
-  $("#imgProfil-9").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-9").attr("src", $changeSrc);
-    $("#nouvelleFenetre-9").css("display", "block");
-  });
-
-  $("#imgProfil-10").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-10").attr("src", $changeSrc);
-    $("#nouvelleFenetre-10").css("display", "block");
-  });
-
-  $("#imgProfil-11").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-11").attr("src", $changeSrc);
-    $("#nouvelleFenetre-11").css("display", "block");
-  });
-
-  $("#imgProfil-12").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-12").attr("src", $changeSrc);
-    $("#nouvelleFenetre-12").css("display", "block");
-  });
-
-  $("#imgProfil-13").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-13").attr("src", $changeSrc);
-    $("#nouvelleFenetre-13").css("display", "block");
-  });
-
-  $("#imgProfil-14").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-14").attr("src", $changeSrc);
-    $("#nouvelleFenetre-14").css("display", "block");
-  });
-
-  $("#imgProfil-15").click(function() {
-    var $changeSrc = $(this).attr("src");
-    $("#testons-15").attr("src", $changeSrc);
-    $("#nouvelleFenetre-15").css("display", "block");
-  });
-
-
 
   closeBtn.click(function() {
     $(".modal").css("display", "none")
   })
 
   $(document).click(function(e) {
-    if (e.target.className == "modal"){
+    if (e.target.className == "modal") {
       $(".modal").css("display", "none")
     }
   });
