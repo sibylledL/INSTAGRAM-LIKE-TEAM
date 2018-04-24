4047da7262dccae4b0af2134d0baa7410fd333f1
sibylledL <33783030+sibylledL@users.noreply.github.com>
2018-04-24 21:59:34 +0200

    test-version comment sibylle

--- insta-js.js
+++ insta-js.js
@@ -8,7 +8,78 @@ $(function() {
     var modal = $(this).data('modal');
     $("#testons-"+modal).attr("src", $changeSrc);
     $("#nouvelleFenetre-"+modal).css("display", "block");
+
+
+    if($("#nouvelleFenetre-"+modal).css("display", "block")){
+
+                  $('.list-emoji'+modal).addClass('.list-emoji');
+                  $('.emoji').css('display','inline');
+                  //var emoji = document.querySelector('.emoji');
+                  //var listEmoji = document.querySelector('.list-emoji');
+                  var count = 1;
+                  $('.list-emoji'+modal).css('display','none');
+
+                  $('.emoji'+modal).click(function(){
+                  //1 - show emoji list on click emoji icone
+                    if(count == 1){
+
+                          $('.list-emoji'+modal).css('display','inline');
+                          $('.list-emoji').css('display','block');
+
+                          $('.list-emoji'+modal).find('span').each(function(){
+                              $(this).click(function(){
+                              var oldValue = $('.newComment-'+modal).val();
+                              var newValue = oldValue + $(this).html();
+                              $('.newComment-'+modal).val(newValue);
+                              });
+                          });
+
+
+                          count = 0;
+
+                    }else if(count==0){
+                          $('.list-emoji'+modal).css('display','none');
+                          count = 1;
+                    }
+
+                  });
+
+
+                  $('.submit'+modal).click(function(){
+
+                    /*var row = table.insertRow(table.lenght);
+                    row.classList.add("row");
+                    var cell1 = row.insertCell(0);
+                    cell1.classList.add("col-1");
+                    var cell2 = row.insertCell(1);
+                    cell2.classList.add("col-2");
+                    cell1.innerHTML = "Rodolphe";
+                    cell2.innerHTML = newComment.value;*/
+
+                    var col1 = "Rodolphe";
+                    var col2 = $('.newComment-'+modal).val();
+                    var tr = "<tr class='row'><td class='col-1'>"+col1+"</td><td class='col-2'>"+col2+"</tr>";
+                    //var node = "<p>jfiorjfire</p>"
+                    $('.table'+modal).append(tr);
+                    $('.newComment-'+modal).val(null);
+
+                    /*setTimeout(function(){
+                      row.classList.remove('blue');}, 1000);*/
+
+                    var tableau = $(".tableau")
+                    var h = tableau.get(0).scrollHeight;
+                    tableau.animate({scrollTop:h}, 300);
+
+                  });
+
+
+
+      }else{
+                  $('.list-emoji'+modal).css('display','none');
+      }
+
   });
+
 
 
   closeBtn.click(function() {
@@ -21,7 +92,12 @@ $(function() {
       $(".modal").css("display", "none")
     }
   });
+
+
+
 });
+
+
 
 //COMMENTAIRES
 
@@ -31,7 +107,10 @@ var newComment = document.querySelector(".newComment");
 var submit = document.querySelector(".submit");
 var table = document.querySelector("table");
 
+
 /*post comment*/
+
+/*
 submit.addEventListener('click',postComment);
 
 function postComment(){
@@ -45,7 +124,11 @@ var cell2 = row.insertCell(1);
 cell2.classList.add("col-2");
 cell1.innerHTML = "Rodolphe";
 cell2.innerHTML = newComment.value;
+
+
+
 /*row.transition="background-color 2s";*/
+/*
 row.classList.add('blue');
 
 setTimeout(function(){
@@ -55,15 +138,14 @@ setTimeout(function(){
 
 newComment.value=null;
 
-
+*//*
 function updateScroll(){
     tableau.scrollTop = tableau.scrollHeight;
-    //tableau.scrollTop=tableau[0];
 }
 setTimeout(updateScroll,300);
+}*/
 
-}
-
+/*
 
 //EMOJI
 //schow and insert emoji ->>>>>
@@ -88,4 +170,4 @@ var icone = Array.from(document.querySelectorAll('.icone'));
 icone.forEach(x => x.addEventListener("click",selectEmoji))
 function selectEmoji(){
 newComment.value += this.innerHTML;
-}
+}*/
