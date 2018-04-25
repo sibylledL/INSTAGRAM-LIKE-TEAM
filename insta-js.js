431ddb77e92b1cf56f1351acc3433668fb5f87e5
mike <mikedorival@gmail.com>
2018-04-25 12:45:39 +0200

    derniere mise a jour

--- insta-js.js
+++ insta-js.js
@@ -1,6 +1,5 @@
 $(function() {
 
-
   var closeBtn = $(".fermeFenetre");
 
   $(".js--img").click(function() {
@@ -20,4 +19,27 @@ $(function() {
       $(".modal").css("display", "none")
     }
   });
+});
+
+// Part Mike
+$( document ).ready(function() {
+  var count = 465;
+  var t = 0;
+  $('.number-likes').text(count + " j'aime");
+
+  $(".btn-icon").click(function(){
+    var aime = $(this).data("aime");
+    if (t == 0) {
+      let lol = count +1;
+      $(".modal-icons-"+ aime).css("color", "#ed4956");
+      $('.number-likes').text(lol + " j'aime");
+      t = 1;
+    }
+    else {
+      let lol = count;
+      $(".modal-icons-"+ aime).css("color", "black");
+      $('.number-likes').text(lol + " j'aime");
+      t = 0;
+    }
+  })
 });
